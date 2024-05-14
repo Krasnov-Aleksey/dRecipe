cdd93162d874b2a9a20d71647ef95ac5c4260901
aleksey.krasnov <krasnov.80@mail.ru>
2024-05-14 11:30:21 +0300

    b1start141130

--- recipeproject/recipeapp/views.py
+++ recipeproject/recipeapp/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
 
 def index(request):
     return HttpResponse('Рецепты')
+
+class Index(TemplateView):
+    template_name = 'recipeapp/base.html'
+
+
